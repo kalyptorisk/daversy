64ff186e02dfb4dcb1b06c281d00f7cd4d6c5427
Ashish Kulkarni <ashish.kulkarni@kalyptorisk.com>
2016-01-27 11:11:34 +0530

    do not bundle the MSVC runtime in the installer
    This can cause stability problems as the runtime DLLs get added to
    the system PATH, which can cause conflicts with other installed
    versions.

--- freeze.py
+++ freeze.py
@@ -3,7 +3,7 @@ import setup, cx_Freeze
 cx_Freeze.setup(
     executables = [cx_Freeze.Executable(script="src/daversy/dvs.py", compress=True),
                    cx_Freeze.Executable(script="src/daversy/tools/dvstool_oracle.py", compress=True)],
-    options     = { 'build_exe': { 'bin_excludes': 'oci.dll', 'include_msvcr': True, 'packages': 'daversy' },
+    options     = { 'build_exe': { 'bin_excludes': 'oci.dll', 'packages': 'daversy' },
                     'bdist_msi': { 'upgrade_code': '{9E829A2E-F5D1-4f14-AE3E-CF9D3D7F60C7}', 'add_to_path': True } },
     **setup.METADATA
 )
